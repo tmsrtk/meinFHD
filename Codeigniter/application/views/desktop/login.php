5db289eca86b646e8e19beeb004515c126d82a82
Peter Jaraczewski <peter@wikipeter.org>
2012-05-30 23:23:56 +0200

    partially ported old login.html to new login.php

--- Codeigniter/application/views/desktop/login.php
+++ Codeigniter/application/views/desktop/login.php
@@ -1,7 +1,58 @@
 <?php include('header.php'); ?>
-
-<h1>Desktop Login View</h1>
-
+<!-- CONTENT -->
+		<!-- CONTENT -->
+	<div class="container container-fluid">
+		<div class="row">
+			<div class="span8">
+				<div class="well well-small clearfix">
+					<!--Titel-->
+					<h1 class="maintitle">meinFHD<span>mobile</span></h1>
+					<hr />
+					
+					<!--Loginform-->
+					<?php print form_open('app/login', array('class' => 'form-inline' )); // create the login form ?>
+				<!--	<form class="form-inline"> -->
+						
+						<input name="user" type="text" class="span2" placeholder="Benutzername" /><br />
+					
+						<input name="pw" type="password" class="span2" placeholder="Passwort" /><br />
+						<hr class="hidden-phone" />
+						
+						<div class="well well-small clearfix">	
+							
+							<!--stay logged in-->
+							<label class="checkbox">
+								<input name="staylogged" type="checkbox" />
+								&nbsp;eingeloggt bleiben
+							</label>
+							
+							<!--Login btn-->
+							<a href="dashboard" class="btn btn-inverse pull-right">
+								<i class="icon-arrow-right icon-white"></i>
+								 anmelden
+							</a>
+						</div>
+					</form>
+				</div>
+			
+			</div><!-- /.span8-->
+			
+			<!--DESKTOP AND TABLET ONLY-->
+			<div class="span4">
+			
+				<div class="well well-small clearfix">
+					<!--Modal trigger Zugang-->
+					<a  class="btn pull-left" data-toggle="modal" href="#accountdata">neuer Zugang</a>
+				
+					<!--Modal trigger pw-->
+					<a class="btn pull-right" data-toggle="modal" href="#accountdata">Passwort vergessen</a>
+				</div>
+			
+			</div><!-- /.span4 -->						
+			
+		</div><!--first row ends here-->
+	</div>
+		<!-- CONTENT ENDE-->
 <?php
 	// create the login form
 	print form_open('app/login');
