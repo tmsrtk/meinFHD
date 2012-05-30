f4c64a8c28f2869ae948e1e6218daa4476d3e6b8
Murat <caglayanm@hotmail.com>
2012-05-30 22:26:06 +0200

    init version of dashboard.php view for mobile and desktop

---
+++ Codeigniter/application/controllers/dashboard.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * meinFHD WebApp
+ * 
+ * @version 0.0.1
+ * @copyright Fachhochschule Duesseldorf, 2012
+ * @link http://www.fh-duesseldorf.de
+ * @author Manuel Moritz (MM), <manuel.moritz@fh-duesseldorf.de>
+ */
+
+/**
+ * Class App
+ *
+ * Description...
+ */
+class Dashboard extends FHD_Controller {
+	
+	/**
+	 * Index
+	 *
+	 * .../dashboard
+	 * .../dashboard/index
+	 */
+	public function index()
+	{
+		$this->load->view('dashboard', $this->data->load());
+	}
+}
+
+/* End of file App.php */
+/* Location: ./application/controllers/App.php */
---
+++ Codeigniter/application/views/desktop/dashboard.php
@@ -0,0 +1,46 @@
+<?php include('header.php'); ?>
+		
+		<!-- CONTENT -->
+		<div class="container container-fluid">
+			<div class="row">
+				<div class="span4">
+					<div class="well widget">
+						<i class="icon icon-question-sign pull-right"></i>
+						<h5><i class="icon icon-tasks"></i>Deine Credit Points</h5>
+						<div class="widget-content">
+							<div id="leistungsContainer" style="width: 278px; height: 70px;"></div>
+						</div>
+					</div>
+				</div><!-- /.span4-->
+				
+				<div class="span4">
+					<div class="well well-small clearfix">
+						<h1 class="clearfix">
+							Heute <small>23.04.2012</small>
+							<a href="stundenplan_simple.html" class="btn btn-large btn-primary pull-right">
+								<i class="icon-arrow-right icon-white"></i>
+							</a>
+						</h1>
+					</div>
+					<div class="well well-small clearfix">
+						<h1 class="clearfix">
+							Wochenplan
+							<a href="stundenplan_table.html" class="btn btn-large btn-primary pull-right">
+								<i class="icon-arrow-right icon-white"></i>
+							</a>
+						</h1>
+					</div>
+				</div><!-- /. span4-->
+				
+				<div class="span4">
+					<div class="well well-small clearfix">
+						<h6>Wilkommen</h6>
+						<!--Titel-->
+						<h1>Dashboard</h1>
+					</div>
+				</div><!-- /.span4-->
+				
+			</div><!--first row ends here-->
+		</div><!-- /.fluid container-->
+		<!-- CONTENT ENDE-->
+<?php include('footer.php'); ?>
