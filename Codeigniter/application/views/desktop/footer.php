33ec0830c5538f790cea6ac0dc3f18725f461e25
Murat <caglayanm@hotmail.com>
2012-05-30 22:25:12 +0200

    corrected head.js functionality within both footer.php files

--- Codeigniter/application/views/desktop/footer.php
+++ Codeigniter/application/views/desktop/footer.php
@@ -1,5 +1,12 @@
-		<!-- only 2 script are loaded directly, the remaining stuff is handled by head.js -->
-		<script src="../resources/jquery/jquery.min.js" type="text/javascript"></script>
-		<script src="../resources/js/meinfhd.js" type="text/javascript"></script>
+		<!-- only head.js is required, due to asynchronous js file handling -->
+		<script src="../resources/headjs/head.min.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			// load scripts and assign labels to them due to better js management
+			head.js( <?php # $javascripFilesAlsArray ?>
+				{jquery: "../resources/jquery/jquery.min.js"},
+				{meinfhd: "../resources/js/meinfhd.js"},
+				{bootstrapAlert: "../resources/bootstrap/js/bootstrap-alert.js"}
+			);
+		</script>
 	</body> <!-- /body -->
 </html> <!-- /html -->
--- Codeigniter/application/views/mobile/footer.php
+++ Codeigniter/application/views/mobile/footer.php
@@ -1,5 +1,13 @@
 		<!-- only 2 script are loaded directly, the remaining stuff is handled by head.js -->
-		<script src="../resources/jquery/jquery.min.js" type="text/javascript"></script>
-		<script src="../resources/js/meinfhd.js" type="text/javascript"></script>
+	
+		<script src="../resources/headjs/head.min.js" type="text/javascript"></script>
+
+		<script type="text/javascript">
+			head.js(
+				{jquery: "../resources/jquery/jquery.min.js"},
+				{meinfhd: "../resources/js/meinfhd.js"},
+				{bootstrapAlert: "../resources/bootstrap/js/bootstrap-alert.js"}
+			);
+		</script>
 	</body> <!-- /body -->
 </html> <!-- /html -->
