c3d87a628f51df940d1b115a0ac649029078ca5d
Fabian Martinovic <jimmy_crowbar@gmx.de>
2012-06-01 14:54:06 +0200

    test.php added (netbeans git-plugin test)

---
+++ Codeigniter/application/models/test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author Fabian
+ */
+class test {
+    //put your code here
+}
+
+?>
