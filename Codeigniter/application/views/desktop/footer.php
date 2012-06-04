d1d38f437b579f8576fcd69c3c558d1352f8f0e5
Manuel Moritz <info@manuelmoritz.de>
2012-06-04 13:51:02 +0200

    Fixed CSS and JS paths in header.php and footer.php

--- Codeigniter/application/views/desktop/footer.php
+++ Codeigniter/application/views/desktop/footer.php
@@ -1,12 +1,12 @@
 		<!-- only head.js is required, due to asynchronous js file handling -->
-		<script src="../resources/headjs/head.min.js" type="text/javascript"></script>
+		<script src="<?php print base_url(); ?>resources/headjs/head.min.js" type="text/javascript"></script>
 		<script type="text/javascript">
 			// load scripts and assign labels to them due to better js management
 			head.js( <?php # $javascripFilesAlsArray ?>
-				{jquery: "../resources/jquery/jquery.min.js"},
-				{meinfhd: "../resources/js/meinfhd.js"},
-				{bootstrap_alert: "../resources/bootstrap/js/bootstrap-alert.js"},
-				{bootstrap_dropdown: "../resources/bootstrap/js/bootstrap-dropdown.js"}
+				{jquery: "<?php print base_url(); ?>resources/jquery/jquery.min.js"},
+				{meinfhd: "<?php print base_url(); ?>resources/js/meinfhd.js"},
+				{bootstrap_alert: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-alert.js"},
+				{bootstrap_dropdown: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-dropdown.js"}
 			);
 		</script>
 	</body> <!-- /body -->
