b25db4a2f5338dba8d7554069729cbcc69ec1d82
kaston <don_kaston@yahoo.de>
2012-06-11 16:31:27 +0200

    - gemerged mit Frank branch
    - Pfade angepasst
    
    - bootstrap Ordner gelöscht, da aus resources die benötigten Daten geladen werden
    
    Signed-off-by: kaston <don_kaston@yahoo.de>

--- Codeigniter/application/views/desktop/includes/header.php
+++ Codeigniter/application/views/desktop/includes/header.php
@@ -4,12 +4,13 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 	<title><?php echo $title?></title>
-	<link rel="stylesheet/less" href="<?php echo base_url(); ?>resources/bootstrap/less/bootstrap.less">
+	<link rel="stylesheet/less" href="<?php echo base_url(); ?>resources/less/meinfhd.less">
 	<script src="<?php echo base_url(); ?>resources/lessjs/less-1.3.0.min.js"></script>
 	<script src="<?php echo base_url(); ?>resources/bootstrap/js/jquery-1.7.2.js"></script>
 	<script src="<?php echo base_url(); ?>resources/bootstrap/js/jquery-ui.js"></script>
 	<script src="<?php echo base_url(); ?>resources/bootstrap/js/bootstrap-dropdown.js"></script>
-	<script src="<?php echo base_url(); ?>resources/bootstrap/js/bootstrap-modal.js"></script>
+	<script src="<?php echo base_url(); ?>
+	resources/bootstrap/js/bootstrap-modal.js"></script>
 </head>
 
 <body>
