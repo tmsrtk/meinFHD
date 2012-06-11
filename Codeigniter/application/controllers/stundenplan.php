f7c87b65677000da8a9ad03c84a2b323c865e706
Simon vom Eyser <simon.v.e@gmx.net>
2012-06-11 13:41:55 +0200

    stundenplan controller edit

--- Codeigniter/application/controllers/stundenplan.php
+++ Codeigniter/application/controllers/stundenplan.php
@@ -1,31 +1,67 @@
-<?php
+<?php   if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * meinFHD WebApp
+ * 
+ * @version 0.0.1
+ * @copyright Fachhochschule Duesseldorf, 2012
+ * @link http://www.fh-duesseldorf.de
+ * @author Simon vom Eyser (SVE), <simon.vomeyser@fh-duesseldorf.de>
+ */
+
+/**
+ * Stundenplan Controller 
+ */
 class Stundenplan extends FHD_Controller {
+    /*
+     * data-array, the whole data concerning the timetable will be stored here in nested arrays.
+     * 
+     * @var Array
+     */
+    private $data;
+    
 	
+	
+
+    /**
+     * Constructor Method
+     * 
+     * @param type name // nicht vorhanden
+     * @return type // nicht vorhanden
+     */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->data['titel'] = "Stundenplan";
+		$this->load->model('Stundenplan_Model');
+	}
+
+/**
+ * 
+ */	
+    /**
+     * Index-method, loads for testing purposes the Stundenplan_view
+     * 
+     * @param type name // nicht vorhanden
+     * @return type // nicht vorhanden
+     */
 	public function index()
-	{
-		//$this->data->add('eigener_key', 'super_value');
-		$this->load->view('stundenplan', $this->data->load());
-	}
 	
-	public function f1()
 	{
-		print 'Funktion 1!';
+		$stundenplan = $this->Stundenplan_Model->get_stundenplan(1384);
+
+		$this->data['tage'] = $stundenplan[0];
+		$this->data['zeiten'] = $stundenplan[1];
+		$this->data['stundenplan'] = $stundenplan[2];
+		$this->data['aktivekurse'] = $stundenplan[3]; //Debug
+
+		//$this->krumo->dump($this->data);
+		$this->load->view('Stundenplan_View', $this->data);
 	}
-	
-	public function f2()
-	{
-		print 'Funktion 2!';
-	}
-	
-	public function f3()
-	{
-		print 'Funktion 3!';
-	}
-	
-	public function mobile_f3()
-	{
-		print 'Mobile Funktion 3!';
-	}
-	
+
 }
+	
+	
+	
+	
+	
