c6edbb06284c88365eeb9cf842fab4fed66c7a7e
frank <f.gottwald@gmail.com>
2012-06-18 15:14:35 +0200

    minor changes - import stdplan

---
+++ Codeigniter/application/views/desktop/admin_stdplan_import.php
@@ -0,0 +1,15 @@
+<div>
+    
+    
+    <?php echo $error; ?>
+    
+    <?php echo form_open_multipart('admin_imports/upload_stdplan'); ?>
+    
+    <input type="file" name="stdplan-import"
+    
+    
+    
+    
+    
+    
+</div>
