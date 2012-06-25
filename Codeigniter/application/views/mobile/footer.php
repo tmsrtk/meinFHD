e31393e6f3c09248dfdc05886a26edf2c15215cd
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-06-25 23:43:08 +0200

    added carousel.js

--- Codeigniter/application/views/mobile/footer.php
+++ Codeigniter/application/views/mobile/footer.php
@@ -7,8 +7,9 @@
 				{meinfhd: "<?php print base_url(); ?>resources/js/meinfhd.js"},
 				{bootstrap_alert: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-alert.js"},
 				{bootstrap_alert: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-collapse.js"},				
-				{bootstrap_dropdown: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-dropdown.js"}
-			);
+				{bootstrap_dropdown: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-dropdown.js"},
+				{bootstrap_dropdown: "<?php print base_url(); ?>resources/bootstrap/js/bootstrap-carousel.js"}				
+			);										
 		</script>
 	</body> <!-- /body -->
 </html> <!-- /html -->
