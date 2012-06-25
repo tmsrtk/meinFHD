de0077a21fdca4a2a1ebc593796b21a7d820c953
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-06-25 21:19:44 +0200

    added functions for accordion icon-toggle

--- Codeigniter/resources/js/meinfhd.js
+++ Codeigniter/resources/js/meinfhd.js
@@ -1,3 +1,18 @@
 (function($) {
 	// custom js functionality goes here
+			        		      			
+	/*icon-switch for accordion-templates*/
+	$(function(){									
+		$('#accordion')
+			.on('hidden', function(e) {
+				
+				$(e.target).parent().find('.accordion-heading i').removeClass('icon-minus').addClass('icon-plus');						
+			})
+			.on('shown', function(e){
+				
+				$(e.target).parent().find('.accordion-heading i').removeClass('icon-plus').addClass('icon-minus');
+		});
+	});
+			
+	
 })(jQuery);
