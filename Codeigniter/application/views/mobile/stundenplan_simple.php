500789f4110827212d146bfecaf7175824484e09
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-06-26 10:45:25 +0200

    tried to get the damn slider working, again

--- Codeigniter/application/views/mobile/stundenplan_simple.php
+++ Codeigniter/application/views/mobile/stundenplan_simple.php
@@ -2,104 +2,116 @@
 
 <!-- CONTENT -->
 <div class="container container-fluid">
+	<div id="carousel" class="carousel slide">
+		<!-- Carousel items -->
+		<div class="carousel-inner">
 			
-	<?php //--------------------Loop for one day-------------------- ?>
-	<?php $day_number = 0 //Incermented after a day ?>
-	<?php foreach ($stundenplan as $dayname => $day) : ?>
-		
-		<!--Tag-->		
-		<div class="row day" id= "<?php echo $dayname ?>">
-			
-			<!--Tagestitel-->
-			<div class="span4">
-				<!--title-->
-				<div class="well well-small well-first">
-		    		<h6>Stundenplan</h6>
-		    		<h1>
-		    			<?php echo $dayname ?>&nbsp;<small><?php echo $tage[$day_number]['Datum'] ?></small>	    		
-		    		</h1>
-	    		</div>				
-    		</div><!-- /.span4, Tagestitel-->
-    		
-    		<!--Tagesinhalte-->								
-			<div class="span8">
-		
-				<div class="accordion well well-small" id="accordion">											
-				<?php //--------------------Loop for one course-------------------- ?>
-				<?php foreach ($day as $hourID => $hour) : ?>
-
-					<?php //--------------------If entry at this hour exists -------------------- ?>
-					<?php if ($hour) { ?>
-	
-						<?php //--------------------Loop for one hour (if many courses)-------------------- ?>
-						<?php foreach ($hour as $courseID => $course) : ?>
-	
-							<?php //--------------------Only if course is active-------------------- ?>
-							<?php if ($course['Aktiv'] == 1) { ?>
-	
-								<!--accordion-group-->	
-								<div class="well-small">
-									<div class="accordion-heading">
-										<table>
-											<tbody>
-												<tr>
-													<td width="45%"><?php echo $course['Beginn']; ?> - <?php echo $course['Ende']; ?></td>
-													<td width="55%"><?php echo $course['kurs_kurz']; ?>&nbsp;<?php echo utf8_decode($course['VeranstaltungsformName']); ?></td>
-													<td>
-														<a class="btn accordion-toggle pull-right" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $course['SPKursID']; ?>">
-															<i class="icon-plus"></i>
-														</a>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-									<div id="<?php echo $course['SPKursID']; ?>" class="accordion-body collapse">
-										<hr />
-										<div class="alert alert-info clearfix">
-										<button class="btn pull-left attendant">
-											<i class="icon-ok"></i>
-											anwesend
-										</button>
-										<a href="modul.html" class="btn btn-primary pull-right">
-											Details
-											<i class="icon-arrow-right icon-white"></i>
-										</a>					
-									</div>
-									</div>
-								</div><!--/accordion-group-->
-	
-	
-							<?php //--------------------End If course is active-------------------- ?>	
-							<?php }  ?>
-	
-						<?php //--------------------End Loop for one hour (if many courses)-------------------- ?>	
-						<?php endforeach; ?>	
-	
-					<?php //--------------------End If entry at this hour exists -------------------- ?>
-					<?php }   ?>
-
-				<?php //--------------------End Loop for one course --------------------?>			
-				<?php endforeach; ?>							
-				</div><!--/#accordion-->			
-			</div><!-- /.span8,Tagesinhalte -->
+				<?php //--------------------Loop for one day-------------------- ?>
+				<?php $day_number = 0 //Incermented after a day ?>
+				<?php foreach ($stundenplan as $dayname => $day) : ?>
+					<div class="item">
+					<!--Tag-->		
+					<div class="row day" id= "<?php echo $dayname ?>">
+						
+						<!--Tagestitel-->
+						<div class="span4">
+							<!--title-->
+							<div class="well well-small well-first">
+					    		<h6>Stundenplan</h6>
+					    		<h1>
+					    			<?php echo $dayname ?>&nbsp;<small><?php echo $tage[$day_number]['Datum'] ?></small>	    		
+					    		</h1>
+				    		</div>				
+			    		</div><!-- /.span4, Tagestitel-->
+			    		
+			    		<!--Tagesinhalte-->								
+						<div class="span8">
 					
-		</div><!-- /.row, Tag-->	
-	
-	<?php $day_number++ ?>	
-	<?php //--------------------End Loop for one day --------------------?>			
-	<?php endforeach; ?>
+							<div class="accordion well well-small" id="accordion">											
+							<?php //--------------------Loop for one course-------------------- ?>
+							<?php foreach ($day as $hourID => $hour) : ?>
+			
+								<?php //--------------------If entry at this hour exists -------------------- ?>
+								<?php if ($hour) { ?>
+				
+									<?php //--------------------Loop for one hour (if many courses)-------------------- ?>
+									<?php foreach ($hour as $courseID => $course) : ?>
+				
+										<?php //--------------------Only if course is active-------------------- ?>
+										<?php if ($course['Aktiv'] == 1) { ?>
+				
+											<!--accordion-group-->	
+											<div class="well-small">
+												<div class="accordion-heading">
+													<table>
+														<tbody>
+															<tr>
+																<td width="45%"><?php echo $course['Beginn']; ?> - <?php echo $course['Ende']; ?></td>
+																<td width="55%"><?php echo $course['kurs_kurz']; ?>&nbsp;<?php echo utf8_decode($course['VeranstaltungsformName']); ?></td>
+																<td>
+																	<a class="btn accordion-toggle pull-right" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $course['SPKursID']; ?>">
+																		<i class="icon-plus"></i>
+																	</a>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+												<div id="<?php echo $course['SPKursID']; ?>" class="accordion-body collapse">
+													<hr />
+													<div class="alert alert-info clearfix">
+													<button class="btn pull-left attendant">
+														<i class="icon-ok"></i>
+														anwesend
+													</button>
+													<a href="modul.html" class="btn btn-primary pull-right">
+														Details
+														<i class="icon-arrow-right icon-white"></i>
+													</a>					
+												</div>
+												</div>
+											</div><!--/accordion-group-->
+				
+				
+										<?php //--------------------End If course is active-------------------- ?>	
+										<?php }  ?>
+				
+									<?php //--------------------End Loop for one hour (if many courses)-------------------- ?>	
+									<?php endforeach; ?>	
+				
+								<?php //--------------------End If entry at this hour exists -------------------- ?>
+								<?php }   ?>
+			
+							<?php //--------------------End Loop for one course --------------------?>			
+							<?php endforeach; ?>							
+							</div><!--/#accordion-->			
+						</div><!-- /.span8,Tagesinhalte -->
+								
+					</div><!-- /.row, Tag-->	
+				</div>
+				<?php $day_number++ ?>	
+				<?php //--------------------End Loop for one day --------------------?>			
+				<?php endforeach; ?>
 
+			
+		</div>
+		<!-- Carousel nav -->
+		<!--<a class="carousel-control left" href="#carousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#carousel" data-slide="next">&rsaquo;</a>-->
+	</div>
+	
 			
 	<div class="row">	
 		<div class="span12">			
 			<div class="pagination pagination-centered">
 			  <ul>
-			    <li><a href="#Montag">M</a></li>
-			    <li><a href="#Dienstag">D</a></li>
-			    <li><a href="#Mittwoch">M</a></li>
-			    <li><a href="#Donnerstag">D</a></li>
-			    <li><a href="#Freitag">F</a></li>					    
+			  	<li><a class="" href="#carousel" data-slide="prev">&lsaquo;</a></li>
+			    <li><a class="slide-montag" href="#Montag">M</a></li>
+			    <li><a class="slide-dienstag" href="#Dienstag">D</a></li>
+			    <li><a class="slide-mittwoch" href="#Mittwoch">M</a></li>
+			    <li><a class="slide-donnerstag" href="#Donnerstag">D</a></li>
+			    <li><a class="slide-freitag" href="#Freitag">F</a></li>	
+			    <li><a class="" href="#carousel" data-slide="next">&rsaquo;</a></li>				    
 			  </ul>
 			</div>			
 		</div><!-- /.span12-->	
