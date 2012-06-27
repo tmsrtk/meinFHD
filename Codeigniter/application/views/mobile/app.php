7c191ebd178d70777558b5eb5d488ba4ca34a3c6
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-06-27 17:53:33 +0200

    imporved füll width buttons

--- Codeigniter/application/views/mobile/app.php
+++ Codeigniter/application/views/mobile/app.php
@@ -10,17 +10,17 @@
     		</div>			
 		</div>
 		<div class="span4">
-			<a class="btn btn-large btn-startpage btn-primary" href="<?php print base_url('stundenplan'); ?>">
-				<strong>Stundenplan</strong>
-				<i class="icon-arrow-right icon-white pull-right"></i>
+			<a class="btn btn-large btn-fullwidth btn-primary" href="<?php print base_url('stundenplan'); ?>">
+				Stundenplan
+				<i class="icon-arrow-right icon-white"></i>
 			</a>
-			<a class="btn btn-large btn-startpage btn-primary" href="<?php print base_url('studienplan'); ?>">
-				<strong>Studienplanung</strong>
-				<i class="icon-arrow-right icon-white pull-right"></i>
+			<a class="btn btn-large btn-fullwidth btn-primary" href="<?php print base_url('studienplan'); ?>">
+				Studienplanung
+				<i class="icon-arrow-right icon-white"></i>
 			</a>
-			<a class="btn btn-large btn-startpage btn-primary" href="<?php print base_url('einstellungen'); ?>">
-				<strong>Einstellungen</strong>
-				<i class="icon-arrow-right icon-white pull-right"></i>
+			<a class="btn btn-large btn-fullwidth btn-primary" href="<?php print base_url('einstellungen'); ?>">
+				Einstellungen
+				<i class="icon-arrow-right icon-white"></i>
 			</a>
 		</div>
 	</div>	
