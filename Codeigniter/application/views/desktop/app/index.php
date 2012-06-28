95333c399268ee9963227cc5b23cb20c3d146ce1
Peter Jaraczewski <peter@wikipeter.org>
2012-06-28 02:20:24 +0200

    desktop/login.php was replaced by desktop/app/login.php

--- Codeigniter/application/views/desktop/app/index.php
+++ Codeigniter/application/views/desktop/app/index.php
@@ -1,5 +1,13 @@
-<?php include('header.php'); ?>
+<?php extend('base/template.php'); # extend main template ?>
 
-<h1>Desktop Application View</h1>
+<?php startblock('preCodeContent'); # additional markup before content ?>
+			<div class="span4"></div>
+			<div class="span4">
+<?php endblock(); ?>
 
-<?php include('footer.php'); ?>
+<?php startblock('postCodeContent'); # additional markup after content ?>
+			</div><!-- /.span4-->
+			<div class="span4"></div>
+<?php endblock(); ?>
+
+<?php end_extend(); # end extend main template ?>
