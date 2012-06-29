c18a1b0ad97a5d6f88409bcb96cf9bb1daf68bc6
Peter Jaraczewski <peter@wikipeter.org>
2012-06-29 15:47:39 +0200

    init new version of global menu (menu per role still missing)

--- Codeigniter/application/views/desktop/base/menu.php
+++ Codeigniter/application/views/desktop/base/menu.php
@@ -1,23 +1,23 @@
-		<div class="navbar navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
-					<a class="brand" href="#">meinFHD<span>mobile</span></a>
-					<div class="nav-collapse">
-						<ul class="nav">
-							<li class="active"><a href="index.html">Dashboard</a></li>
-							<li><a href="#">Studienplanung</a></li>
-							<li><a href="#">Persönliche Daten</a></li>
-							<li><a href="#">Hilfe</a></li>
-							<li><a href="#">Impressum</a></li>
-							<!--LOGOUT-->
-							<li><a href="#">Logout</a></li>
-						</ul>
-					</div><!--/.nav-collapse -->
+			<nav class="navbar navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						<a class="brand" href="#">meinFHD<span>mobile</span></a>
+						<div class="nav-collapse">
+							<ul class="nav">
+								<li class="active"><a href="index.html">Dashboard</a></li>
+								<li><a href="#">Studienplanung</a></li>
+								<li><a href="#">Persönliche Daten</a></li>
+								<li><a href="#">Hilfe</a></li>
+								<li><a href="#">Impressum</a></li>
+								<!--LOGOUT-->
+								<li><a href="app/logout">Logout</a></li>
+							</ul>
+						</div><!--/.nav-collapse -->
+					</div>
 				</div>
-			</div>
-		</div>
+			</nav>
