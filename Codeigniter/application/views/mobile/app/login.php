39078226ec03273e1ca1ab44a03390b3596a8e28
Peter Jaraczewski <peter@wikipeter.org>
2012-07-05 14:29:39 +0200

    refactored login box

--- Codeigniter/application/views/mobile/app/login.php
+++ Codeigniter/application/views/mobile/app/login.php
@@ -2,50 +2,97 @@
 
 <?php startblock('title'); # extend the site's title ?><?php get_extended_block(); ?> - Login<?php endblock();?>
 
+<?php
+# general form setup: login-form
+$loginFormAttributes = array(
+	'class' 		=> 'login-form'
+);
+
+// prepare attributes for username input field 
+$usernameInputAttributes = array(
+	'name'			=> 'username',
+	'id'			=> 'username',
+	'placeholder'	=> 'Benutzername',
+	'class'			=> 'input-xxlarge'
+);
+
+// prepare attributes for username input field 
+$passwordInputAttributes = array(
+	'name'			=> 'password',
+	'id'			=> 'password',
+	'placeholder'	=> 'Passwort',
+	'class'			=> 'input-xxlarge'
+);
+
+// prepare attributes for permanent login checkbox 
+$permanentLoginAttributes = array(
+	'name'			=> 'permanentLogin',
+	'id'			=> 'permanentLogin',
+	'class'			=> ''
+);
+
+// prepare attributes for submit button 
+$submitLoginButtonAttributes = array(
+	'name'			=> 'submit',
+	'type'			=> 'submit',
+	'id'			=> 'submitLoginForm',
+	'content'			=> 'Anmelden',
+	'class'			=> 'btn btn-primary btn-medium pull-right'
+);
+
+
+
+# general form setup: request-account-form
+$requestAccountFormAttributes = array(
+	'class' 		=> 'request-account-form'
+);
+
+// prepare attributes for forgot-password email input field 
+$requestAccountInputAttributes = array(
+	'name'			=> 'request-account',
+	'id'			=> 'request-account',
+	'placeholder'	=> 'E-Mail',
+	'class'			=> 'input-large'
+);
+
+// prepare attributes for submit button 
+$submitRequestAccountButtonAttributes = array(
+	'name'			=> 'submit',
+	'type'			=> 'submit',
+	'id'			=> 'submitRequestAccountForm',
+	'content'			=> 'Anfordern',
+	'class'			=> 'btn btn-primary btn-small'
+);
+
+
+
+# general form setup: forgot-password-form
+$forgotPasswordFormAttributes = array(
+	'class' 		=> 'forgot-password-form'
+);
+
+// prepare attributes for forgot-password email input field 
+$forgotInputAttributes = array(
+	'name'			=> 'forgot-email',
+	'id'			=> 'forgot-email',
+	'placeholder'	=> 'E-Mail',
+	'class'			=> 'input-medium'
+);
+
+// prepare attributes for submit button 
+$submitForgotPasswordButtonAttributes = array(
+	'name'			=> 'submit',
+	'type'			=> 'submit',
+	'id'			=> 'submitForgotPasswordForm',
+	'content'			=> 'Senden',
+	'class'			=> 'btn btn-primary btn-small'
+);
+?>
+
 <?php startblock('content'); # content for this view ?>
-				<?php //print $global_data['messages']; ?>
 				<div class="well well-small clearfix">
-					<h1 class="maintitle">Login</h1>
-					<hr />
-					<?php
-					#	$this->load->library('message');
-						
-						$loginFormAttributes = array(
-							'class' 		=> 'login-form'
-						);
-						
-						// prepare attributes for username input field 
-						$usernameInputAttributes = array(
-							'name'			=> 'username',
-							'id'			=> 'username',
-							'placeholder'	=> 'Benutzername',
-							'class'			=> 'input-xxlarge'
-						);
-						
-						// prepare attributes for username input field 
-						$passwordInputAttributes = array(
-							'name'			=> 'password',
-							'id'			=> 'password',
-							'placeholder'	=> 'Passwort',
-							'class'			=> 'input-xxlarge'
-						);
-						
-						// prepare attributes for permanent login checkbox 
-						$permanentLoginAttributes = array(
-							'name'			=> 'permanentLogin',
-							'id'			=> 'permanentLogin',
-							'class'			=> ''
-						);
-						
-						// prepare attributes for submit button 
-						$submitButtonAttributes = array(
-							'name'			=> 'submit',
-							'type'			=> 'submit',
-							'id'			=> 'submitLoginForm',
-							'content'			=> 'Anmelden',
-							'class'			=> 'btn btn-primary pull-right'
-						);
-					?>
+					<h1>Login</h1>
+					<hr>
 					<?php echo form_open('app/login', $loginFormAttributes ); // create opening tag of login form ?>
 						<?php echo form_fieldset(); // wrap elements ina a fieldset due to semantics ?>
 							<div class="control-group">
@@ -70,49 +117,57 @@
 									</label>
 								</div>
 							</div>
-							
-							<hr/>
-							<div class="control-group">
-								<div class="controls">
-									<div class="btn-group dropdown pull-left">
-										
-										<button class="btn"><i class="icon-question-sign"></i> Hilfe</button>
-										<button class="btn dropdown-toggle" data-toggle="dropdown">
-											<span class="caret"></span>
-										</button>
-										<ul class="dropdown-menu">
-											<!--Modal trigger Zugang-->
-											<li><a  class="" data-toggle="modal" href="#accountdata"><i class="icon-user"></i> Zugang anfordern</a></li>
-											<!--Modal trigger pw-->
-											<li><a class="" data-toggle="modal" href="#request-password"><i class="icon-envelope"></i> Passwort vergessen</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-								
-							<?php echo form_button($submitButtonAttributes); // render the submit button ?>
+							<hr>
+							<?php echo form_button($submitLoginButtonAttributes); // render the submit button ?>
 						<?php echo form_fieldset_close(); // close the fieldset ?>
 					<?php echo form_close(); // close the whole login form ?>
+				</div>
+				<div id="accordion-app" class="accordion">
+					<div class="accordion-group">
+						<div class="accordion-heading">
+							<h4 class="accordion-toggle" data-parent="#accordion-app" data-toggle="collapse" data-target="#request-content">Zugang anfordern  <i class="icon-plus pull-right"></i></h4>
+						</div>
+						<div id="request-content" class="accordion-body collapse">
+							<div class="accordion-inner">
+								<div class="row-fluid">
+								<p>Kein Problem! Bitte trage deine E-Mail Adresse ein, das Passwort wird anschließend zugestellt.</p>
+								<?php echo form_open('app/request_account', $requestAccountFormAttributes ); // create opening tag of login form ?>
+									<div class="control-group">
+										<div class="controls">
+											<div class="input-prepend input-append">
+												<span class="add-on">@</span><?php echo form_input($requestAccountInputAttributes); // render the forgot-password email field ?><?php echo form_button($submitRequestAccountButtonAttributes); ?>
+											</div>
+										</div>
+									</div>
+								<?php echo form_close(); // close the whole login form ?>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="accordion-group">
+						<div class="accordion-heading">
+							<h4 class="accordion-toggle" data-parent="#accordion-app" data-toggle="collapse" data-target="#forgot-content">Passwort vergessen <i class="icon-plus pull-right"></i></h4>
+						</div>
+						<div id="forgot-content" class="accordion-body collapse">
+							<div class="accordion-inner">
+								<p>Kein Problem! Bitte trage deine E-Mail Adresse ein, das Passwort wird anschließend zugestellt.</p>
+								<?php echo form_open('app/forgot', $forgotPasswordFormAttributes ); // create opening tag of login form ?>
+									<div class="control-group">
+										<div class="controls">
+											<div class="input-prepend input-append">
+												<span class="add-on">@</span><?php echo form_input($forgotInputAttributes); // render the forgot-password email field ?><?php echo form_button($submitForgotPasswordButtonAttributes); ?>
+											</div>
+										</div>
+									</div>
+								<?php echo form_close(); // close the whole password forgot form ?>
+							</div>
+						</div>
+					</div>
 				</div>
 <?php endblock(); ?>
 
 <?php startblock('postCodeFooter'); # use for hidden markup like modals ?>
-		<!-- REQUEST PASSWORD MODAL OVERLAY-->
-		<div class="modal fade" id="request-password">
-			<div class="modal-header">
-				<h3>Passwort vergessen?</h3>
-			</div>
-			<div class="modal-body">
-				<p>Kein Problem! Bitte trage deine E-Mail Adresse ein, das Passwort wird anschließend zugestellt.</p>
-				<label for="email">E-mail-Adresse</label>
-				<input name="email" type="text" />
-			</div>
-			<div class="modal-footer">
-				<a href="#" data-dismiss="modal" class="btn btn-small">schließen</a>
-				<a href="#" class="btn btn-primary btn-small">absenden</a>
-			</div>
-		</div>
-		<!-- REQUEST PASSWORD MODAL OVERLAY ends here-->
+
 <?php endblock(); ?>
 
 
