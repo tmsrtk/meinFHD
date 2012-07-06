6ba18db3cb1a9da84877289daaa9568622074c52
Peter Jaraczewski <peter@wikipeter.org>
2012-07-06 17:40:45 +0200

    restored old test.php for FireBHP logging test

--- Codeigniter/application/views/desktop/test.php
+++ Codeigniter/application/views/desktop/test.php
@@ -1,12 +1,1 @@
-<?php 
-#krumo($this->firephploader);
-$this->firephp->log($userdata);
-#	krumo($this->firephp);
-	
-#	$firephp->log($userdata);
-#	krumo($firephp);
-#	
-#	FB::log($userdata);
-echo 'test';
-$this->firephploader->hello();
-?>
+<?php FB::log($userdata); ?>
