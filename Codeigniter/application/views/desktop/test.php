6a46674c503d2d2731a01eb4b9cf9bdda310ac9f
Peter Jaraczewski <peter@wikipeter.org>
2012-07-06 09:47:14 +0200

    resolved conflict in autoload

--- Codeigniter/application/views/desktop/test.php
+++ Codeigniter/application/views/desktop/test.php
@@ -1,1 +1,12 @@
-<?php FB::log($userdata) ?>
+<?php 
+#krumo($this->firephploader);
+$this->firephp->log($userdata);
+#	krumo($this->firephp);
+	
+#	$firephp->log($userdata);
+#	krumo($firephp);
+#	
+#	FB::log($userdata);
+echo 'test';
+$this->firephploader->hello();
+?>
