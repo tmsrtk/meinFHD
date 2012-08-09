a2740175e552fff92a4b1a47a7b628111685d4b6
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-08-09 20:22:49 +0200

    Modifications in app controller for providing automatic authentication if the user is authenticated at the idp, has got an linked account in the local user db and visits the main page of the meinfhd.

--- Codeigniter/application/controllers/app.php
+++ Codeigniter/application/controllers/app.php
@@ -19,6 +19,7 @@ class App extends FHD_Controller {
 	// default constructor to prepare all needed stuff
 	function __construct(){
 		parent::__construct();
+
 	}
 	
 	/**
@@ -37,35 +38,46 @@ class App extends FHD_Controller {
 	 *
 	 * .../app/login
 	 * .../login
+     * Modifications and minor changes by Christian Kundruss (CK), 2012
 	 */
 	public function login()
 	{
-		// read the post parameters
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		
-		// if we have a value
-		if ($username || $password)
-		{
-			// call the login funtion from the authentication class
-			if ($this->authentication->login($username, $password))
-			{
-				// user is logged in -> set message and redirect to frontpage
-				$this->message->set(sprintf('Eingeloggt! (ID: %s)', $this->authentication->user_id()));
-				redirect('dashboard/index');
-			}
-			else
-			{
-				// something got wrong -> set message and redirect to login page
-				$this->message->set('User oder Passwort falsch!', 'error');
-				redirect('app/login');
-			}
-		}
-		
+        // --- Modification for automatic authentication if an global session exists Begin ---
+        // a global session exists and the user has got an linked account -> log him in
+        if ($this->samlauthentication->is_authenticated()) {
+            // redirect zum sso controller
+            redirect('sso/authenticate_linked_account');
+        }
+        // --- Modification End ---
+
+        // there is no global session & no linked account -> show the local login page when visiting the page
+        else {
+            // read the post parameters
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            // if we have a value
+            if ($username || $password)
+            {
+                // call the login funtion from the authentication class
+                if ($this->authentication->login($username, $password))
+                {
+                    // user is logged in -> set message and redirect to frontpage
+                    $this->message->set(sprintf('Eingeloggt! (ID: %s)', $this->authentication->user_id()));
+                    redirect('dashboard/index');
+                }
+                else
+                {
+                    // something got wrong -> set message and redirect to login page
+                    $this->message->set('User oder Passwort falsch!', 'error');
+                    redirect('app/login');
+                }
+            }
+        }
 		// if there's no post data, we should show the login screen
 		$this->load->view('app/login', $this->data->load());
 	}
-	
+
 	/**
 	 * Logout
 	 *
@@ -75,8 +87,7 @@ class App extends FHD_Controller {
 	 */
 	public function logout()
 	{
-        echo $this->authentication->isLoggedInFromAdmin();
-
+        // --- Modification for returning to admin session, if the admin is logged in as another user Begin ---
         if ($this->authentication->isLoggedInFromAdmin() == 'TRUE') { // the user is logged in from an admin, change the user data and switch back to admin session
             $this->authentication->switchBackToAdmin();
             // show a message that the return into the admin session was successful
@@ -84,6 +95,7 @@ class App extends FHD_Controller {
             // redirect to the 'benutzerverwaltung/benutzer bearbeiten'; outgoing point
             redirect('admin/edit_user_mask');
         }
+        // --- Main Modification End ---
 
         else { // otherwise perform a regular logut
             $this->message->set(sprintf('Ausgeloggt! (ID: %s)', $this->authentication->user_id()));
