f100582ecb13e8a59d1960befd07dc80cce6533e
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-08-12 18:57:08 +0200

    Fixed validation errors in account linking view

--- Codeigniter/application/views/desktop/sso/link_account.php
+++ Codeigniter/application/views/desktop/sso/link_account.php
@@ -55,30 +55,31 @@ $submitLinkAccountButtonAttributes = array(
                 <div id="accordion-app" class="accordion">
                     <div class="accordion-group">
                         <div class="accordion-heading">
-                            <h4 class="accordion-toggle" data-parent="#accordion-app" data-toggle="collapse" data-target="#request-content">Ja, ich m&ouml;chte meinen Account verkn&uuml;pfen  <i class="icon-plus pull-right"></i></h4>
+                            <h4 class="accordion-toggle" data-parent="#accordion-app" data-toggle="collapse" data-target="#request-content">Ja, ich besitze einen Accound und m&ouml;chte diesen jetzt verkn&uuml;pfen<i class="icon-plus pull-right"></i></h4>
                         </div>
                         <div id="request-content" class="accordion-body collapse">
                             <div class="accordion-inner">
                                 <div class="row-fluid">
                                     <p>Bitte trage deinen Loginnamen und dein Passwort ein. Anschließend wird der angegebene lokale Zugang mit deiner globalen Identit&auml;t verkn&uuml;pft und du wirst eingeloggt.</p>
                                     <?php echo form_open('sso/link_account', $linkAccountFormAttributes); // create opening tag of login form ?>
-                                    <div class="control-group">
-                                        <div class="controls">
-                                            <div class="input-prepend">
-                                                <span class="add-on"><i class="icon-user"></i></span><?php echo form_input($usernameInputAttributes); // render the username field ?>
+                                        <?php echo form_fieldset(); // wrap elements ina a fieldset due to semantics ?>
+                                            <div class="control-group">
+                                                <div class="controls">
+                                                    <div class="input-prepend">
+                                                        <span class="add-on"><i class="icon-user"></i></span><?php echo form_input($usernameInputAttributes); // render the username field ?>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="control-group">
-                                        <div class="controls">
-                                            <div class="input-prepend">
-                                                <span class="add-on"><i class="icon-lock"></i></span><?php echo form_password($passwordInputAttributes); // render the password field ?>
+                                            <div class="control-group">
+                                                <div class="controls">
+                                                    <div class="input-prepend">
+                                                        <span class="add-on"><i class="icon-lock"></i></span><?php echo form_password($passwordInputAttributes); // render the password field ?>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <?php echo form_button($submitLinkAccountButtonAttributes); // render the submit button ?>
-                                    <?php echo form_fieldset_close(); // close the fieldset ?>
+                                            <hr>
+                                            <?php echo form_button($submitLinkAccountButtonAttributes); // render the submit button ?>
+                                        <?php echo form_fieldset_close(); // close the fieldset ?>
                                     <?php echo form_close(); // close the whole login form ?>
                                 </div>
                             </div>
