2a4a30a26fef7c62fcfb3cf7b2c32bb81372320b
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-08-18 18:58:30 +0200

    Added oid attribute filter to cast attributes to cleartext keys in the idp attribute array. Removed unnecessary idp configurations.

--- Codeigniter/application/libraries/simplesamlphp/metadata/saml20-idp-remote.php
+++ Codeigniter/application/libraries/simplesamlphp/metadata/saml20-idp-remote.php
@@ -91,88 +91,15 @@ iLC5QkJG1V/W/8slZ/DPwDrL+g6PKvbXHrg6x1HmcY8BLV/hDioQGBA/qmd0xdsA
     array (
         0 => 'example.org',
     ),
-);
 
-
-/*
- * Guest IdP. allows users to sign up and register. Great for testing!
- */
-$metadata['https://openidp.feide.no'] = array(
-	'name' => array(
-		'en' => 'Feide OpenIdP - guest users',
-		'no' => 'Feide Gjestebrukere',
-	),
-	'description'          => 'Here you can login with your account on Feide RnD OpenID. If you do not already have an account on this identity provider, you can create a new one by following the create new account link and follow the instructions.',
-
-	'SingleSignOnService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'      => 'c9ed4dfb07caf13fc21e0fec1572047eb8a7a4cb'
-);
-
-
-/*
- * Feide, the norwegian federation. Test and production metadata.
- */
-$metadata['https://idp-test.feide.no'] = array(
-	'name' => array(
-		'en' => 'Feide Test environment',
-		'no' => 'Feide testmiljø',
-	),
-	'description'                  => 'Feide test environment (idp-test.feide.no). Authenticate with your identity from a school or university in Norway.',
-
-	'SingleSignOnService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-
-	'certFingerprint'              => 'fa982efdb69f26e8073c8f815a82a0c5885960a2',
-	'hint.cidr'                    => '158.38.0.0/16',
-);
-
-$metadata['https://idp.feide.no'] = array(
-	'name' => 'Feide',
-	'description' => array(
-		'en' => 'Authenticate with your identity from a school or university in Norway.',
-		'no' => 'Logg inn med din identitet fra skolen eller universitetet du er tilknyttet (i Norge).',
-	),
-	'SingleSignOnService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'              => 'cde69e332fa7dd0eaa99ee0ddf06916e8942ac53',
-	'hint.cidr'                    => '158.38.0.0/16',
-);
-
-
-
-/*
- * Wayf, the danish federation metadata.
- */
-$metadata['https://wayf.wayf.dk'] = array(
-	'name' => array(
-		'en' => 'DK-WAYF Production server',
-		'da' => 'DK-WAYF Produktionsmiljøet',
-	),
-	'description'          => 'Login with your identity from a danish school, university or library.',
-	'SingleSignOnService'  => 'https://wayf.wayf.dk/saml2/idp/SSOService.php',
-	'SingleLogoutService'  => 'https://wayf.wayf.dk/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'      => 'c215d7bf9d51c7805055239f66b957d9a72ff44b'
-);
-
-$metadata['https://betawayf.wayf.dk'] = array(
-	'name' => array(
-		'en' => 'DK-WAYF Quality Assurance',
-		'da' => 'DK-WAYF Quality Assurance miljøet',
-	),
-	'description'          => 'Login with your identity from a danish school, university or library.',
-	'SingleSignOnService'  => 'https://betawayf.wayf.dk/saml2/idp/SSOService.php',
-	'SingleLogoutService'  => 'https://betawayf.wayf.dk/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'      => 'c215d7bf9d51c7805055239f66b957d9a72ff44b'
-);
-
-$metadata['https://testidp.wayf.dk'] = array(
-	'name' => array(
-		'en' => 'DK-WAYF Test Server',
-		'da' => 'DK-WAYF Test Miljøet',
-	),
-	'description'          => 'Login with your identity from a danish school, university or library.',
-	'SingleSignOnService'  => 'https://testidp.wayf.dk/saml2/idp/SSOService.php',
-	'SingleLogoutService'  => 'https://testidp.wayf.dk/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'      => '04b3b08bce004c27458b3e85b125273e67ef062b'
-);
+    /* Enable the authproc filter below to add URN Prefixces to all attributes */
+    /*
+     * Enable the authproc filter to remove oid and urn prefixes from all attributes to have cleartext attributnames
+     * For further information of the attribute names see /attributemap/oid2name.php.
+     */
+       'authproc' => array(
+           10 => array(
+               'class' => 'core:AttributeMap', 'oid2name'
+           ),
+       ),
+   );
