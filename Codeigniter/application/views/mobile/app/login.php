77be67f23d2157dbf302d35b89ae3e6e2520063e
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-09-13 22:54:12 +0200

    Adjusted login mask.

--- Codeigniter/application/views/mobile/app/login.php
+++ Codeigniter/application/views/mobile/app/login.php
@@ -36,7 +36,7 @@ $submitLoginButtonAttributes = array(
     'name'			=> 'submit',
     'type'			=> 'submit',
     'id'			=> 'submitLoginForm',
-    'content'			=> 'Anmelden',
+    'content'			=> 'lokal Anmelden',
     'class'			=> 'btn btn-primary btn-medium pull-right'
 );
 
@@ -121,10 +121,10 @@ $submitForgotPasswordButtonAttributes = array(
     <?php echo form_button($submitLoginButtonAttributes); // render the submit button ?>
     <?php echo form_fieldset_close(); // close the fieldset ?>
     <?php echo form_close(); // close the whole login form ?>
-    <hr>
     <div class="row-fluid">
-        <a href="<?php echo site_url(); ?>sso/authenticate">Login &uuml;ber Shibboleth</a>
+        <a href="<?php echo site_url(); ?>sso/authenticate" class="btn btn-primary btn-small pull-right">Login &uuml;ber Shibboleth</a>
     </div>
+    <hr>
 </div>
 <div id="accordion-app" class="accordion">
     <div class="accordion-group">
