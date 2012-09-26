5a6975f32d4e373defbcb429c4e08b9e2c7ea732
Konny <konstantin.voth@googlemail.com>
2012-09-26 14:40:20 +0200

    outsorced the Bootstrap Modal functions out of single files to meinfhd.js, to to be able to use them globally. added comments.

--- Codeigniter/resources/js/meinfhd.js
+++ Codeigniter/resources/js/meinfhd.js
@@ -63,6 +63,75 @@
 		});
 		
 	});
-			
-	
+
+
+
+
 })(jQuery);
+
+
+/**
+ * Creates the needed HTML Markup for the desired Bootstrap Modal.
+ * @author Konstantin Voth
+ * @param  {String} title  Title of the modal.
+ * @param  {String} text   Message in the modal.
+ * @param  {bool} withOK Should there be an OK button?
+ * @return {jQuery Object}
+ * @private
+ */
+function _createModalDialog(title, text, withOK) {
+	myModalDialog =
+		$('<div class="modal hide" id="myModal"></div>')
+		.html('<div class="modal-header"><button type="button" class="close" data-dismiss="modal">×</button><h3>'+title+'</h3></div>')
+		.append('<div class="modal-body"><p>'+text+'</p></div>')
+		.append('<div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Schließen</a>');
+		if (withOK) myModalDialog.find('.modal-footer').append('<a href="" class="btn btn-primary" data-accept="modal">OK</a></div>');
+		// <a href="" class="btn btn-primary" data-accept="modal">OK</a></div>
+	return myModalDialog;
+}
+
+/**
+ * Shows the Bootstrap Modal. There have to be additional HTML markup in your view file to
+ * show the modal correctly. Place <div id="modalcontent"></div> at the bottom of your view file.
+ * @author Konstantin Voth
+ * @param   {String} title  Title of the dialog.
+ * @param   {[type]} text   Message in the modal.
+ * @param   {[type]} withOK Should there be an OK button?
+ * @private
+ */
+function _showModal(title, text, withOK) {
+	mm = _createModalDialog(title, text, withOK);
+	$('#modalcontent').html(mm);
+
+	$('#myModal').modal({
+		keyboard: false
+	}).on('hide', function () {
+		$("input[type=submit][data-clicked=true]").removeAttr("data-clicked");
+	}).modal('show');
+
+	if (withOK) {
+		// if there are any click listener, remove them
+		$('#modalcontent').off('click');
+		// add new
+		$("#modalcontent").on( 'click', 'button, a', function(event) {
+			event.preventDefault();
+
+			if ( $(this).attr("data-accept") === 'modal' ) {
+				console.log("accept");
+
+				$(event.target).parent().parent().find("div.modal-body").html("Bitte warten, der Befehl wird ausgeführt");
+				$(event.target).parent().parent().find("div.modal-footer").hide();
+
+				// get the form name dynamically, to be able to use this in every form
+				form_id = $("input[type=submit][data-clicked=true]").parents("form").attr("id");
+				// console.log(form_id);
+
+				$("input[type=submit][data-clicked=true]").parents("form#"+form_id).submit();
+
+			} else {
+				console.log("cancel");
+			}
+
+		});
+	}
+}
