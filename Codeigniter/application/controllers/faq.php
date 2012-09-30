186f14b3d0b762b2c5d8572087ca6d6eed951bfa
Manuel Moritz <manuelmoritz1308@googlemail.com>
2012-09-30 18:39:22 +0200

    FAQ, Hilfe and Studienplan update with data from fabian

--- Codeigniter/application/controllers/faq.php
+++ Codeigniter/application/controllers/faq.php
@@ -1,4 +1,7 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 /**
  * meinFHD WebApp
@@ -6,32 +9,22 @@
  * @version 0.0.1
  * @copyright Fachhochschule Duesseldorf, 2012
  * @link http://www.fh-duesseldorf.de
- * @author Peter Jaraczewski (PJ), <peter.jaraczewski@fh-duesseldorf.de>
+ * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
  */
 
 /**
- * Class Faq
- *
- * Description...
+ * Hilfe-Controller
  */
-class Faq extends FHD_Controller {
-	
-	// default constructor to prepare all needed stuff
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Index
-	 *
-	 * .../faq
-	 * .../faq/index
-	 */
-	public function index()
-	{
-		$this->load->view('faq/index', $this->data->load());
-	}
+class FAQ extends FHD_Controller {
+
+    /**
+     * Index
+     */
+    public function index() {
+        $this->load->view('faq/index', $this->data->load());
+    }
+
 }
 
-/* End of file faq.php */
-/* Location: ./Application/Controllers/faq.php */
+/* End of file App.php */
+/* Location: ./application/controllers/faq.php */
--- Codeigniter/application/controllers/hilfe.php
+++ Codeigniter/application/controllers/hilfe.php
@@ -1,4 +1,7 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 /**
  * meinFHD WebApp
@@ -6,32 +9,22 @@
  * @version 0.0.1
  * @copyright Fachhochschule Duesseldorf, 2012
  * @link http://www.fh-duesseldorf.de
- * @author Peter Jaraczewski (PJ), <peter.jaraczewski@fh-duesseldorf.de>
+ * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
  */
 
 /**
- * Class Hilfe
- *
- * Description...
+ * Hilfe-Controller
  */
 class Hilfe extends FHD_Controller {
-	
-	// default constructor to prepare all needed stuff
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Index
-	 *
-	 * .../hilfe
-	 * .../hilfe/index
-	 */
-	public function index()
-	{
-		$this->load->view('hilfe/index', $this->data->load());
-	}
+
+    /**
+     * Index
+     */
+    public function index() {
+        $this->load->view('hilfe/index', $this->data->load());
+    }
+
 }
 
-/* End of file hilfe.php */
-/* Location: ./Application/Controllers/hilfe.php */
+/* End of file App.php */
+/* Location: ./application/controllers/hilfe.php */
