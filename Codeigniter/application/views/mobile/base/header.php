6779e750c60c4ab9a2408dad8b7171bbc81e970c
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-09-30 22:20:00 +0200

    disabled zoom

--- Codeigniter/application/views/mobile/base/header.php
+++ Codeigniter/application/views/mobile/base/header.php
@@ -6,6 +6,7 @@
 		<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="HandheldFriendly" content="true" />
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		
 		<!-- Dynamic title -->
