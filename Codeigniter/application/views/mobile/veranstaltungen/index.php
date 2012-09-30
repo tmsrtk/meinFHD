e1416ed3dd3b414f2c3cf5c6d18bfae9ba3dcd2b
Johannes Hagenberg <johanneshagenberg@googlemail.com>
2012-09-30 22:46:52 +0200

    added table-like design to fit global style

--- Codeigniter/application/views/mobile/veranstaltungen/index.php
+++ Codeigniter/application/views/mobile/veranstaltungen/index.php
@@ -6,34 +6,44 @@
 <!- ------------------------------------------------ -->
 <!-- Veranstaltungen -------------------------------- -->
 <!- ------------------------------------------------ -->
+<div class="row-fluid">
+	<div class="span6 well">
 
-		<div class="span4">
-			<div class="well well-small clearfix">
+		<h6>Veranstaltungsliste</h6>
+		<h1>Semester <?php echo $userdata["act_semester"]; ?></h1>
+
+		<?php $alte_kursID = 1; ?>
 		
-				<h6>Veranstaltungsliste</h6>
+		<table class="table table-condensed" width="100%">
+			<thead>
+				<tr>
+					<th width="85%">Veranstaltung</th>
+					<th>Details</th>					
+				</tr>
+			</thead>
+			<tbody>
 		
-				<!--Titel-->
-    			<h1>Semester <?php echo $userdata["act_semester"]; ?></h1>
-				<hr />
-
-					<?php $alte_kursID = 1; ?>
-
-					<?php foreach ($kurse as $kurs => $value): ?>
-
-					<?php if ($value['KursID'] != $alte_kursID): ?>
-
-							<a href="<?php echo base_url('modul/show/' . $value['KursID']); ?>" class="btn btn-large btn-fullwidth btn-primary"> <?php echo $value['Kursname']; ?><i class="icon-ok icon-white"></i></a>
-
-					<?php endif ?>
-
-					<?php $alte_kursID = $value['KursID']; ?>
-
-					<?php endforeach ?>
-			    		    	   									  
-			</div>
-			
-		</div>
-										
+			<?php foreach ($kurse as $kurs => $value): ?>
+	
+				<?php if ($value['KursID'] != $alte_kursID): ?>
+					<tr>
+						<td><?php echo $value['Kursname']; ?></td>
+						<td>
+							<a href="<?php echo base_url('modul/show/' . $value['KursID']); ?>" class="btn btn-primary pull-right">
+								<i class="icon-arrow-right icon-white"></i>
+							</a>
+						</td>						
+					</tr>
+				<?php endif ?>
+	
+				<?php $alte_kursID = $value['KursID']; ?>
+	
+			<?php endforeach ?>
+			</tbody>
+		</table>
+		    		    	   									  	
+	</div>
+</div>										
 
 <?php endblock(); ?>
 <?php end_extend(); ?>
