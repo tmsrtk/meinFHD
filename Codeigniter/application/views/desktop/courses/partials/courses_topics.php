42dd86041f4dc7f592108cda652b56ae1deed9fc
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-10-04 01:21:54 +0200

    Removed semicolon separation option from topic management.

--- Codeigniter/application/views/desktop/courses/partials/courses_topics.php
+++ Codeigniter/application/views/desktop/courses/partials/courses_topics.php
@@ -14,7 +14,7 @@
 
 <h3>Themen</h3>
 <p>Hier k&ouml;nnen Sie die Themen der Veranstaltung in kurzen Stichworten auff&uuml;hren. Diese k&ouml;nnen die Studenten zur eigenen Lernstandskontrolle
-   verwenden.<br/>Jede Zeile wird dabei als ein Thema aufgefasst, oder aber Sie trennen die einzelnen Themen per Semikolon voneinander (Bsp. Thema 1; Thema 2; ...).
+   verwenden.<br/>Jede Zeile wird dabei als ein Thema aufgefasst.
 </p>
 <div>
     <?php
