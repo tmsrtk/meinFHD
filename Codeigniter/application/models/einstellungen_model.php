a7b515dbe7c9e3beea9cfc855e9c85b0d20d1b63
Konny <konstantin.voth@googlemail.com>
2012-10-16 10:12:39 +0200

    persönliche Einstellungen - password bugfix

--- Codeigniter/application/models/einstellungen_model.php
+++ Codeigniter/application/models/einstellungen_model.php
@@ -19,13 +19,15 @@ class Einstellungen_model extends CI_Model {
 
 		$update = array(
 			'LoginName'				=> $formdata['loginname'],
-			'Passwort'				=> md5($formdata['password']),
 			'Vorname'				=> $formdata['forename'],
 			'Nachname'				=> $formdata['lastname'],
 			'Email'					=> $formdata['email'],
 			'StudienbeginnSemestertyp'	=> $formdata['semesteranfang'],
 			'StudienbeginnJahr'		=> $formdata['startjahr']
 			);
+		if ( ! empty($formdata['password'])) $update['Passwort'] = md5($formdata['password']);
+
+		// FB::log($update);
 
 		$this->db->where('BenutzerID', $this->user_model->get_userid());
 		$this->db->update('benutzer', $update);
