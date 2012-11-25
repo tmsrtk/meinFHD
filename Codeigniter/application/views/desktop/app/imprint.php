ffed0f28b56fefcf2da689181cf32e8d4b7a455d
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-11-25 23:06:12 +0100

    Embedded new data privacy statement

--- Codeigniter/application/views/desktop/app/imprint.php
+++ Codeigniter/application/views/desktop/app/imprint.php
@@ -36,24 +36,33 @@
         <div id="datenschutz">
             <h3>Datenschutzerkl&auml;rung</h3>
             <hr/>
-
             <h4>Sinn und Zweck des Web-Angebots meinFHD</h4>
-            <p>Das Web-Angebot meinFHD soll sie bei der Planung Ihres Studiums und Ihrer Stundenplanung unterst&uuml;tzen sowie den Informationsflu&szlig; von Dozenten zu Studierenden erleichtern. Den Fachbereich unterst&uuml;tzt das Angebot bei der notwendigen Organisation von Gruppen f&uuml;r &Uuml;bungen, Praktika, Seminare, etc sowie durch automatisch erstellte (anonyme) E-Mail-Verteiler.</p>
+            <p>Das Web-Angebot meinFHD soll Sie bei der Planung Ihres Studiums und Ihrer Stundenplanung unterst&uuml;tzen sowie den Informationsflu&szlig; von DozentInnen und MitarbeiterInnen zu Studierenden erleichtern. Den Fachbereich, die DozentInnen und die MitarbeiterInnen unterst&uuml;tzt das meinFHD bei der notwendigen Organisation von Gruppen f&uuml;r &Uuml;bungen, Praktika, Seminare, etc. sowie durch automatisch erstellte anonyme E-Mail-Verteiler.</p>
 
             <h4>Personenbezogene Daten</h4>
-            <p>Alle personenbezogenen Daten geben Sie selber ein. Wichtig f&uuml;r die Organisation sind f&uuml;r den Fachbereich Ihr Name und eine E-Mail-Adresse, unter der Sie wirklich st&auml;ndig erreichbar sind.</p>
+            <p>Alle personenbezogenen Daten Ihres Accounts geben Sie selber ein. Wichtig f&uuml;r die Organisation sind f&uuml;r den Fachbereich Ihr Name und eine E-Mail-Adresse, unter der Sie wirklich st&auml;ndig erreichbar sind.</p>
             <p>Ihr Name wird gebraucht, um Sie eindeutig in einer Gruppe (&Uuml;bungsgruppe, ...) identifizieren zu k&ouml;nnen. Au&szlig;erdem kann so gepr&uuml;ft werden, ob sie in dieser Gruppe eine Pr&uuml;fungsvorleistung erbracht haben. Sie sollten also Ihren korrekten Namen angeben.</p>
-            <p>An die angegebene E-Mail-Adresse werden im Verlaufe des Semesters von Dozenten Aufgaben, Hinweise und andere Informationen verschickt. Sie sollten also eine Adresse verwenden, unter der Sie auch erreichbar sind. E-Mail-Adressen werden automatisch zu anonymen Verteilern zusammengestellt (z.B. alle H&ouml;rer der Vorlesung Mathematik 1 im Studiengang BSc Medieninformatik), die teilweise f&uuml;r alle Nutzer verf&uuml;gbar sind. Die Teilnehmer der Verteiler sind nicht sichtbar und daher anonym.</p>
-            <p>Ihre Matrikelnummer k&ouml;nnen Sie zus&auml;tzlich angeben. Sie erleichtern den Dozenten und Betreuern damit die Organisation der Lehre.<p>Alle weiteren Daten und Informationen, die Sie in meinFHD eingeben, sind ausschliesslich zu Ihrer eigenen Information und zur Organisation Ihres eigenen Studienverlaufs bestimmt. Sie werden weder von uns kontrolliert, noch kann ein anderer Studierender oder Dozent diese einsehen.</p>
+            <p>An die von Ihnen angegebene E-Mail-Adresse werden im Verlaufe des Semesters von Dozenten Aufgaben, Hinweise und andere Informationen verschickt. Sie sollten also eine Adresse verwenden, unter der Sie auch erreichbar sind. E-Mail-Adressen werden automatisch zu anonymen Verteilern zusammengestellt (z.B. alle H&ouml;rer der Vorlesung Mathematik 1 im Studiengang BSc Medieninformatik), die teilweise f&uuml;r alle Nutzer verf&uuml;gbar sind. Die Teilnehmer der Verteiler sind nicht sichtbar und daher anonym.</p>
+            <p>Ihre Matrikelnummer k&ouml;nnen Sie zus&auml;tzlich angeben. Sie erleichtern den DozentInnen und BetreuerInnen damit sehr die Organisation der Lehre.<p>Alle weiteren Daten und Informationen, die Sie in meinFHD eingeben, sind ausschlie&szlig;lich zu Ihrer eigenen Information und zur Organisation Ihres eigenen Studienverlaufs bestimmt. Sie werden weder von uns kontrolliert, noch kann ein anderer Studierender oder Dozent diese einsehen.</p>
+
+            <h4>Freiwilligkeit Ihrer Nutzung und &Auml;ndern und L&ouml;schen Ihrer Daten</h4>
+            <p>Falls Sie <strong>ab</strong> dem Wintersemester <strong>2012/13</strong> Ihren aktuellen Studiengang an der FH D&uuml;sseldorf aufgenommen haben gilt:
+            <ul>
+                <li>F&uuml;r die Unterst&uuml;tzung der Studienorganisation in ihrem Studiengang am FB Medien sind Sie auf Grundlage von &sect; 13 Abs. 3 Satz 4 Nr. 2 der Einschreibungordnung der FH D vom 3.7.2012 verpflichtet den Dienst meinFHD zu nutzen. </li>
+                <li>Alle Daten, die uns &uuml;ber unsere Website mitgeteilt worden sind, werden nur so lange gespeichert, bis der Zweck - die Organisation Ihres Studiums - erf&uuml;llt ist. Da Sie selber Zugriff zu allen Ihren Daten haben, k&ouml;nnen Sie diese jederzeit selber &auml;ndern und auch l&ouml;schen - sofern das nicht dem oben angegebenen Zweck zuwider l&auml;uft. Beachten Sie dabei, dass Sie selber f&uuml;r die Richtigkeit der Daten verantwortlich sind. </li>
+            </ul>
+            <p>Falls Sie <strong>vor</strong> dem Wintersemester <strong>2012/13</strong> Ihren aktuellen Studiengang an der FH D&uuml;sseldorf aufgenommen haben gilt: </p>
+            <ul>
+                <li>Sie nutzen zur Unterst&uuml;tzung der Studienorganisation in ihrem Studiengang am FB Medien freiwillig den Dienst meinFHD, Ihre Einwilligung k&ouml;nnen Sie jederzeit widerrufen. Im Falle einer Nicht-Zustimmung bzw. eines Widerrufs werden Ihnen alternative Zug&auml;nge zu studien- bzw. pr&uuml;fungsrelevanten Unterlagen bereitgestellt. Dies gilt insbesondere f&uuml;r Pflichtveranstaltungen. Wenden Sie sich im Fall der Nicht-Zustimmung bzw. eines Widerrufs bitte umgehend an die jeweilige Dozentin/an den jeweiligen Dozenten, um einen alternativen Weg zu den Unterlagen zu erhalten.</li>
+                <li>Alle Daten, die uns &uuml;ber unsere Website mitgeteilt worden sind, werden nur so lange gespeichert, bis der Zweck - die Organisation Ihres Studiums - erf&uuml;llt ist. Da Sie selber Zugriff zu allen Ihren Daten haben, k&ouml;nnen Sie diese jederzeit &auml;ndern und auch l&ouml;schen. Beachten Sie dabei, dass Sie selber f&uuml;r die Richtigkeit der Daten verantwortlich sind. </li>
+            </ul>
 
             <h4>Keine Weitergabe der Daten</h4>
-            <p>Wir geben Ihre personenbezogenen Daten im &Uuml;brigen nicht an Dritte weiter, es sei denn, dass wir dazu gesetzlich verpflichtet w&auml;ren oder Sie vorher ausdr&uuml;cklich eingewilligt haben. Soweit wir zur Durchf&uuml;hrung und Abwicklung von Verarbeitungsprozessen Dienstleistungen Dritter in Anspruch nehmen, werden die Bestimmungen des Bundesdatenschutzgesetzes eingehalten.</p>
+            <p>Wir, die FH D&uuml;sseldorf, geben Ihre personenbezogenen Daten im &Uuml;brigen nicht an Dritte weiter, es sei denn, dass wir dazu gesetzlich verpflichtet w&auml;ren oder Sie vorher ausdr&uuml;cklich eingewilligt haben. Soweit wir zur Durchf&uuml;hrung und Abwicklung von Verarbeitungsprozessen Dienstleistungen Dritter in Anspruch nehmen, werden die Bestimmungen des Bundesdatenschutzgesetzes eingehalten.</p>
 
             <h4>L&ouml;schen von Daten und Account</h4>
-            <p>Alle Daten, die uns &uuml;ber unsere Website mitgeteilt worden sind, werden nur so lange gespeichert, bis der Zweck erf&uuml;llt ist. Da Sie selber Zugriff zu allen Ihren Daten haben, k&ouml;nnen Sie diese jederzeit &auml;ndern und auch l&ouml;schen. Beachten Sie dabei, dass Sie selber f&uuml;r die Richtigkeit der Daten verantwortlich sind.</p>
-            <p>Wenn Sie Ihren Account in meinFHD komplett entfernen m&ouml;chten, schicken Sie bitte eine formlose Mitteilung an das Dekanat des Fachbereichs.</p>
-            <p>Wenn Sie gegen die allgemeinen Nutzungsbedingungen von IT im Fachbereich versto&szlig;en, beh&auml;lt sich der Fachbereich vor, Ihren Account zu l&ouml;schen.</p>
-            <p>Wenn Sie starke Einw&auml;nde gegen die Nutzung von meinFHD haben, nehmen Sie mit dem Fachbereich Kontakt auf.</p>
+            <p>Wenn Sie Ihren Account in meinFHD komplett entfernen m&ouml;chten, schicken Sie bitte eine formlose Mitteilung an die Kontakt-Email-Adresse, die auf der Webseite von meinFHD angegeben ist.
+                Wenn Sie gegen die allgemeinen Nutzungsbedingungen von IT im Fachbereich versto&szlig;en, beh&auml;lt sich der Fachbereich vor, Ihren Account zu l&ouml;schen.</p>
         </div>
     </div>
 </div>
