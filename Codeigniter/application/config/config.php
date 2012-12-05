306bcaddd5f494ab422772e8ec284d9bc30432d9
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-12-06 00:37:46 +0100

    Changed name of the cookie to 'meinFHD'.

--- Codeigniter/application/config/config.php
+++ Codeigniter/application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = '7b4cba38e080cb7298f62f2138a7a967717468277f470f0206d
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'meinFHD';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
