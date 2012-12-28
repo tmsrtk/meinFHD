9aa7bf9deeb24e6e2ab613b68e92f0659ec58048
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-12-28 02:15:31 +0100

    Documentating

--- Codeigniter/application/libraries/static/Roles.php
+++ Codeigniter/application/libraries/static/Roles.php
@@ -1,15 +1,43 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
+/**
+ * meinFHD WebApp
+ *
+ * @version 0.0.2
+ * @copyright Fachhochschule Duesseldorf, 2012
+ * @link http://www.fh-duesseldorf.de
+ * @author Christian Kundruß(CK), <christian.kundruss@fh-duesseldorf.de>
+ */
 
 /**
-* Holding static role properties.
+* Class Roles
+* Holds static role properties.
 */
-class Roles
-{
-	CONST ADMIN 		= 1;
-	CONST DOZENT 		= 2;
-	CONST BETREUER 	= 3;
-	CONST TUTOR 		= 4;
-	CONST STUDENT 	= 5;
+class Roles{
+
+    /**
+     * Constant that maps the admin role id to an named constant. Easier access.
+     */
+	CONST ADMIN = 1;
+
+    /**
+     * Constant that maps the dozent role id to an named constant. Easier access.
+     */
+    CONST DOZENT = 2;
+
+    /**
+     * Constant that maps the betreuer role id to an named constant. Easier access.
+     */
+    CONST BETREUER = 3;
+
+    /**
+     * Constant that maps the tutor role id to an named constant. Easier access.
+     */
+    CONST TUTOR = 4;
+
+    /**
+     * Constant that maps the student role id to an named constant. Easier access.
+     */
+    CONST STUDENT = 5;
 }
+/* End of file Roles.php */
+/* Location: ./application/libraries/static/Roles.php */
