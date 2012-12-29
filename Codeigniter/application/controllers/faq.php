2a6104b7f6d4eb1f5a9896d28517755e0adf1cf1
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2012-12-29 02:40:17 +0100

    Refactoring and redesign of faq views for desktop and mobile version. Views were invalid (W3C…). Additional controller documentation.

--- Codeigniter/application/controllers/faq.php
+++ Codeigniter/application/controllers/faq.php
@@ -1,7 +1,4 @@
-<?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * meinFHD WebApp
@@ -13,27 +10,35 @@ if (!defined('BASEPATH'))
  */
 
 /**
- * Hilfe-Controller
+ * FAQ-Controller
+ *
+ * Controller with all necessary functions for the frequently asked questions.
  */
 class FAQ extends FHD_Controller {
 	
-	// default constructor to prepare all needed stuff
-	function __construct(){
+    /**
+     * Default constructor. Used for initialization.
+     *
+     * @access public
+     * @return void
+     */
+	public function __construct(){
 		parent::__construct();
-
 	}
 	
 	/**
-	 * Index
-	 *
+	 * Loads the faq index view.
+	 * Function is usually called when the following url patterns are submitted:
 	 * .../faq
 	 * .../faq/index
+     *
+     * @access public
+     * @return void
 	 */
 	public function index()
 	{
 		$this->load->view('faq/index', $this->data->load());
 	}
 }
-
-/* End of file App.php */
+/* End of file faq.php */
 /* Location: ./application/controllers/faq.php */
--- Codeigniter/application/views/mobile/faq/index.php
+++ Codeigniter/application/views/mobile/faq/index.php
@@ -1,14 +1,14 @@
 <?php extend('base/template.php'); # extend main template  ?>
 
-<?php startblock('title'); # extend the site's title ?><?php get_extended_block(); ?> FAQ<?php endblock(); ?>
+<?php startblock('title'); # extend the site's title ?><?php get_extended_block(); ?> - FAQ<?php endblock(); ?>
 
 <?php startblock('content'); # content for this view ?>
 
 <?php
-// the statements below are the data for FAQ. If we have enough time, please move them into the database.
+// the statements below are the data for FAQ.
 $faq = array();
 
-// $faq[X][0] means "Title"	
+// $faq[X][0] means title/asked question
 $faq[0][0] = 'Warum soll ich mein Studium jetzt schon planen?';
 $faq[1][0] = 'Wie kann ich mein Studium jetzt schon planen?';
 $faq[2][0] = 'Wie kann ich mich für ein Praktikum anmelden?';
@@ -20,68 +20,51 @@ $faq[7][0] = 'Wie kann ich eine Gruppe wechseln?';
 $faq[8][0] = 'Muss ich alle Veranstaltungen des Stundenplans besuchen?';
 $faq[9][0] = 'An wen wende ich mich, wenn ich weitere Fragen zu meinFHD habe?';
 
-// $faq[X][1] means "Content belongs to the Title"		
+// $faq[X][1] means "Content belongs to the title/question"
 $faq[0][1] = 'Damit Du direkt einen guten Überblick über die Kurse und Prüfungen, die auf Dich zukommen, hast.';
-$faq[1][1] = 'Indem Du einen Semesterplan anlegst und ihn individuell einrichtest. Das kannst du über den Hauptmenüpunkt "Studium planen" tun.';
-$faq[2][1] = '<p>Indem du im "<strong><a href="stundenplan_simple.html">Stundenplan</a></strong>" auf den Link "anmelden" der jeweiligen Praktikumsgruppe klickst und anschließend Deine nderungen speicherst. Voraussetzung dafür ist aber, dass Du vorher einen Studiengang und Deinen Studienbeginn in Deinen persönlichen Daten angegeben hast.</p>';
-$faq[3][1] = 'Diese Entscheidung liegt bei Dir. Aber Du solltest Deine Gruppen so planene, dass sie sich nicht überkreuzen und Du an allen notwendigen Gruppen teilnehmen kannst.';
+$faq[1][1] = 'Indem Du einen Semesterplan anlegst und ihn individuell einrichtest. Das kannst du über den Hauptmenüpunkt <strong>"Studienplan"</strong> tun.';
+$faq[2][1] = 'Indem du im "<strong>Stundenplan</strong>" auf den Link "anmelden" der jeweiligen Praktikumsgruppe klickst und anschließend Deine Änderungen speicherst. Voraussetzung dafür ist aber, dass Du vorher einen Studiengang und Deinen Studienbeginn in Deinen persönlichen Daten angegeben hast.';
+$faq[3][1] = 'Diese Entscheidung liegt bei Dir. Aber Du solltest Deine Gruppen so planen, dass sie sich nicht überkreuzen und Du an allen notwendigen Gruppen teilnehmen kannst.';
 $faq[4][1] = 'Für Praktika musst Du Dich immer anmelden. Übungen sind optional. An dem An-/Abmelde-Link jeder Veranstaltung kannst Du sehen, ob du bereits für <strong>diese</strong> Veranstaltung angemeldet bist.';
 $faq[5][1] = 'Einmal an der Gruppenbezeichnung. Wenn zwei Veranstaltungen z.B. <strong>OOP1 P2a</strong> heißen, gehören sie zusammen. Nachdem Du Dich für eine Veranstaltung angemeldet hast, werden alle Praktika-Alternativen komplett ausgeblendet und alle Übungs- und Seminar-Alternativen heller gefärbt.';
 $faq[6][1] = 'Dann bist du über die meinFHD-Email-Verteiler nicht erreichbar und bekommst, im Falle eines nicht angemeldeten Praktikums, keine Zulassung zur Klausur.';
 $faq[7][1] = 'Indem Du Dich von der einen Gruppe abmeldest und Dich dann bei einer anderen Gruppe wieder anmeldest.';
 $faq[8][1] = 'Praktika sind die einzigen Veranstaltungen die Du besuchen <strong>musst</strong>. Vorlesungen, Übungen und Seminare sind freiwillig.';
-$faq[9][1] = 'Per Email an <strong><a href="mailto:meinFHD.medien@fh-duesseldorf.de">meinFHD.medien@fh-duesseldorf.de</a></strong>.<br>Vielleicht findest Du auch die passende Antwort in der detaillierten Hilfe.';
+$faq[9][1] = 'Per Email an <strong><a href="mailto:meinFHD.medien@fh-duesseldorf.de">meinFHD.medien@fh-duesseldorf.de</a></strong>.<br/>Vielleicht findest Du auch die passende Antwort in der detaillierten Hilfe.';
 ?>
 
+<div class="well well-small">
+    <div class="row-fluid">
+        <div class="span12">
+            <h6>Fragen und Antworten</h6>
+            <h1>FAQ</h1>
+        </div>
+    </div>
+    <hr/>
+    <div class="row-fluid">
+        Auf dieser Seite sind häufig auftretende Fragen und die dazugehörigen Antworten notiert, um so bei konkreten Fragen schneller helfen zu können.
+    </div>
+    <br/>
 
-<div class="row-fluid">
 
-	<div class="span4 well">
-		<h6>Fragen und Antworten</h6>
-		<h1>FAQ</h1>
-	</div>
-
-	<div class="span8">
-		<?php for ($n = 0; $n < count($faq); $n++): ?>
-			
-			<div class="accordion">
-				<div class="accordion-group">
-					<!-- begin : FAQ title -->
-					<div class="accordion-heading">
-						<div class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse_<?php echo $n; ?>">
-							<?php echo $faq[$n][0]; ?>
-							<i class="icon-plus pull-right"></i>
-						</div>
-					</div>
-					<!-- end : FAQ title -->
-
-					<!-- begin : FAQ content -->
-					<div id="collapse_<?php echo $n; ?>" class="accordion-body collapse">
-						<div class="accordion-inner">
-							<div class="alert alert-info clearfix">
-								<?php echo $faq[$n][1]; ?>
-							</div>
-						</div>
-					</div>
-					<!-- begin : FAQ content-->
-				</div>
-			</div>
-		<?php endfor; ?>
-
-	</div>
-	<!-- end : the FAQ list -->
+    <div class="container-fluid">
+        <div class="accordion" id="faqAccordion">
+            <?php for ($n = 0; $n < count($faq); $n++): // for each defined question (see above) print the accordion?>
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <h4 class="accordion-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#<?php echo $n; ?>"><?php echo $faq[$n][0]; ?><i class="icon-plus pull-right"></i></h4>
+                </div>
+                <div id="<?php echo $n; ?>" class="accordion-body collapse">
+                    <div class="accordion-inner">
+                        <div class="alert alert-info clearfix">
+                            <?php echo $faq[$n][1]; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endfor; ?>
+        </div>
+    </div>
 </div>
-<!-- end : the first row -->
-
-
-<div class="row-fluid">
-	<div class="span12">
-		<div class="fhd-box">
-			<a href="<?php print base_url('dashboard/mobile'); ?>" class="btn btn-large btn-primary">Übersicht</a>
-			<a href="<?php print base_url('hilfe'); ?>" class="btn btn-large pull-right">Hilfe</a>
-		</div>
-	</div>
-</div>
-
 <?php endblock(); ?>
 <?php end_extend(); # end extend main template  ?>
