e1487a098911c3cd3edc26bdc925dc477108c0c8
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2013-04-02 00:54:44 +0200

    Deleted prototype and deprecated views.

--- Codeigniter/application/views/desktop/admin_create_user_success.php
+++
@@ -1,5 +0,0 @@
-<h2>Benutzer wurde erfolgreich erstellt</h2>
-
-<h2>Benutzer wurde erfolgreich geloescht</h2>
-
-<h2>Benutzer wurde erfolgreich geändert</h2>
--- Codeigniter/application/views/desktop/stundenplan.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php'); ?>
-
-<h1>Desktop Stundenplan View</h1>
-
-<?php include('footer.php'); ?>
