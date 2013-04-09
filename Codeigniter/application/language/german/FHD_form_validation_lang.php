f6d054e9f6001d7bb6928bcbab421f04cbaf7c85
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2013-04-10 00:16:39 +0200

    Added custom german language pack to provide german error messages and changed language configuration to be 'german'.

---
+++ Codeigniter/application/language/german/FHD_form_validation_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['alpha_dash_space'] = "Das Feld %s darf nur Buchstaben, Zahlen, Bindestriche und Unterstriche enthalten.";
+
+/* End of file FHD_form_validation_lang.php */
+/* Location: ./system/language/german/FHD_form_validation_lang.php */
