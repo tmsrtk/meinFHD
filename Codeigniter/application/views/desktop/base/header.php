8444c94d59c06985a462973c3791adb5097ff23e
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2013-08-14 00:23:10 +0200

    Integrated default fb5 favicon in mobile and desktop version.

--- Codeigniter/application/views/desktop/base/header.php
+++ Codeigniter/application/views/desktop/base/header.php
@@ -2,6 +2,8 @@
 <html lang="de">
 	<head>
 		<!-- Meta stuff -->
+        <link rel="shortcut icon" href="<?php echo site_url('/resources/favicon/favicon.ico');?>" type="image/x-icon">
+        <link rel="icon" href="<?php echo site_url('/resources/favicon/favicon.ico');?>" type="image/x-icon">
 		<meta charset="UTF-8">
 		<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,10 +14,6 @@
 		<title><?php start_block_marker('title'); # breadcrumb like title-tag ?>meinFHD<?php end_block_marker(); ?></title>
 		
 		<!-- Styles -->
-		<!--
-		<link rel="stylesheet/less" type="text/css" href="../resources/bootstrap/less/bootstrap.less">
-		<link rel="stylesheet/less" type="text/css" href="../resources/bootstrap/less/responsive.less">
-		-->
 		<?php $this->less->auto_compile_less(array('meinfhd.less','meinfhd-responsive.less')); // autocompile files ?>
 <?php start_block_marker('headerCss'); ?>
 		<link rel="stylesheet" type="text/css" href="<?php print base_url(); ?>resources/css/meinfhd.css">
@@ -30,19 +28,10 @@
 
 	</head> <!-- /head -->
 	<body>
-		<!-- First header has an ID so you can give it individual styles, and target stuff inside it -->
 		<header id="header">
-<?php start_block_marker('menu'); ?>
-			<!-- Main nav, styled by targeting "#header nav"; you can have more than one nav element per page -->
-<?php $this->load->view('base/menu'); ?>
-<?php end_block_marker(); ?>
-<?php start_block_marker('hgroup'); # hgroup tags can be used ?>
-			<!-- "hgroup" is used to make two headings into one, to prevent a new document node from forming -->
-			<!--
-			<hgroup>
-			<h1>Easy HTML5 Template</h1>
-			<h2>tagline</h2>
-			</hgroup>
-			-->
-<?php end_block_marker(); ?>
+            <?php start_block_marker('menu'); ?>
+                <?php $this->load->view('base/menu'); ?>
+            <?php end_block_marker(); ?>
+            <?php start_block_marker('hgroup'); # hgroup tags can be used ?>
+            <?php end_block_marker(); ?>
 		</header><!-- #header -->
