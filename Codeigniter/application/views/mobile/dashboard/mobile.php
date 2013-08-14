b0a5d4b98062b24d2582684184d7387df11adf87
Christian Kundruss <christian.kundruss@fh-duesseldorf.de>
2013-08-14 09:07:57 +0200

    Role based "mobile" dashboard / landing page.

--- Codeigniter/application/views/mobile/dashboard/mobile.php
+++ Codeigniter/application/views/mobile/dashboard/mobile.php
@@ -1,17 +1,29 @@
 <?php extend('base/template.php'); # extend main template ?>
+<?php startblock('title'); # extend the site's title ?>
+    <?php get_extended_block(); ?> - &Uuml;bersicht
+<?php endblock();?>
 
-	<?php startblock('title'); # extend the site's title ?>
-		<?php get_extended_block(); ?> - Übersicht
-	<?php endblock();?>
-	
-	<?php startblock('content'); # content for this view ?>
-		
-		<div class="span4 well">
-			<a href="<?php print base_url('stundenplan'); ?>" class="mfhd-btn-block-primary">Stundenplan</a>
-			<a href="<?php print base_url('studienplan'); ?>" class="mfhd-btn-block-primary">Studienplan</a>
-			<a href="<?php print base_url('dashboard'); ?>" class="mfhd-btn-block-primary">Dashboard</a>
-			<a href="<?php print base_url('veranstaltungen'); ?>" class="mfhd-btn-block-primary">Meine Kurse</a>
-			<a href="<?php print base_url('logout'); ?>" class="mfhd-btn-block-inverse">Abmelden</a>
+<?php startblock('content'); # content for this view ?>
+
+		<div class="span4 offset4 well well-small">
+            <?php if($this->authentication->has_permissions('hat_stundenplan')): ?>
+            <a href="<?php print base_url('stundenplan'); ?>" class="mfhd-btn-block-primary">Stundenplan</a>
+            <?php endif; ?>
+            <?php if($this->authentication->has_permissions('hat_semesterplan')): ?>
+            <a href="<?php print base_url('studienplan'); ?>" class="mfhd-btn-block-primary">Studienplan</a>
+		    <?php endif; ?>
+            <?php if($this->authentication->has_permissions('hat_kurse')): ?>
+            <a href="<?php print base_url('kursverwaltung/show_coursemgt'); ?>" class="mfhd-btn-block-primary">Kursverwaltung</a>
+            <?php endif; ?>
+            <?php if ( $this->authentication->has_permissions('hat_logbuch') ) : ?>
+<!--                <a href="--><?php //print base_url('logbuch/index'); ?><!--" class="mfhd-btn-block-primary">Logbuch</a>-->
+            <?php endif; ?>
+            <?php if ( $this->authentication->is_logged_in() ) : ?>
+                <a href="<?php print base_url('einstellungen'); ?>" class="mfhd-btn-block-primary">Einstellungen</a>
+            <?php endif; ?>
+            <?php if ( $this->authentication->is_logged_in() ) : ?>
+            <a href="<?php print base_url('logout'); ?>" class="mfhd-btn-block-inverse">Logout</a>
+            <?php endif; ?>
 		</div>
 					
 	<?php endblock(); ?>
